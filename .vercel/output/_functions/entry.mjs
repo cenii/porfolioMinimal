77c2983a1f575de2d81056f35781fee02cdbc0c6
Hugo Ceniceros <hcenicerosolave@gmail.com>
2025-12-18 12:47:36 +0100

    ok

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CIc-7vzt.mjs';
-import { manifest } from './manifest_JZvc0pt7.mjs';
+import { manifest } from './manifest_CG-gTLCU.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -21,7 +21,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "c82a10d0-394b-4972-8f44-46901392cecd",
+    "middlewareSecret": "4842907d-6051-4c17-9467-a72c855e60c3",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
